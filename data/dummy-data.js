24667ac9a427cabb76c43ffcdcb438a9b19ebb0a
Logan Thimer <thimerlogan@gmail.com>
2023-08-23 07:02:36 -0600

    Dummy test data

---
+++ data/dummy-data.js
@@ -0,0 +1,74 @@
+export const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-08-05"),
+  },
+  {
+    id: "e2",
+    description: "Groceries",
+    amount: 85.2,
+    date: new Date("2023-08-04"),
+  },
+  {
+    id: "e3",
+    description: "Pet supplies",
+    amount: 45.67,
+    date: new Date("2023-08-03"),
+  },
+  {
+    id: "e4",
+    description: "Gas for car",
+    amount: 40.0,
+    date: new Date("2023-08-02"),
+  },
+  {
+    id: "e5",
+    description: "Dinner at restaurant",
+    amount: 70.1,
+    date: new Date("2023-08-01"),
+  },
+  {
+    id: "e6",
+    description: "Movie tickets",
+    amount: 30.0,
+    date: new Date("2023-07-24"),
+  },
+  {
+    id: "e7",
+    description: "New books",
+    amount: 20.0,
+    date: new Date("2023-07-21"),
+  },
+  {
+    id: "e8",
+    description: "Monthly Gym Membership",
+    amount: 30.0,
+    date: new Date("2023-07-29"),
+  },
+  {
+    id: "e9",
+    description: "Public Transport Tickets",
+    amount: 20.5,
+    date: new Date("2023-07-28"),
+  },
+  {
+    id: "e10",
+    description: "New Headphones",
+    amount: 120.0,
+    date: new Date("2023-07-27"),
+  },
+  {
+    id: "e11",
+    description: "Medical Expenses",
+    amount: 200.5,
+    date: new Date("2023-07-26"),
+  },
+  {
+    id: "e12",
+    description: "Coffee Shop",
+    amount: 15.3,
+    date: new Date("2023-07-25"),
+  },
+];
