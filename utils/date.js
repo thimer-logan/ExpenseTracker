808a700747d63a33444ca489467a1ad2fbbc18db
Logan Thimer <thimerlogan@gmail.com>
2023-08-30 07:10:10 -0600

    Added helper function for filtering by days

--- utils/date.js
+++ utils/date.js
@@ -5,3 +5,20 @@ export function getFormattedDate(date) {
 export function getDateMinusDays(date, days) {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
 }
+
+export function getMinusDaysFromFilter(filter) {
+  if (filter === "day") {
+    return 0;
+  }
+  if (filter === "week") {
+    return 7;
+  }
+  if (filter === "month") {
+    return 30;
+  }
+  if (filter === "year") {
+    return 365;
+  }
+
+  return 0;
+}
