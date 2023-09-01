13feffa2438347f5adbad85d097a4bb2245edd5d
Logan Thimer <thimerlogan@gmail.com>
2023-09-01 16:30:40 -0600

    Removed expense summary

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,31 +3,20 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-export default function ExpensesOutput({
-  expenses,
-  expensesPeriod,
-  fallbackText,
-}) {
+export default function ExpensesOutput({ style, expenses, fallbackText }) {
   let content = <Text style={styles.infoText}>{fallbackText}</Text>;
 
   if (expenses.length > 0) {
     content = <ExpensesList expenses={expenses} />;
   }
 
-  return (
-    <View style={styles.container}>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      {content}
-    </View>
-  );
+  return <View style={[styles.container, style]}>{content}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 24,
-    paddingTop: 24,
-    paddingBottom: 0,
+
     backgroundColor: GlobalStyles.colors.background.primary,
   },
   infoText: {
