744a65072e933c2e326e44cae6c63e947f1ee989
Logan Thimer <thimerlogan@gmail.com>
2023-11-04 10:17:16 -0600

    Added more colors to color palette

--- constants/styles.js
+++ constants/styles.js
@@ -1,32 +1,43 @@
 export const GlobalStyles = {
   colors: {
-    primary50: "#e4d9fd",
-    primary100: "#c6affc",
-    primary200: "#a281f0",
-    primary400: "#5721d4",
-    primary500: "#3e04c3",
-    primary700: "#2d0689",
-    primary800: "#200364",
-    accent500: "#f7bc0c",
     error50: "#fcc4e4",
     error300: "#ff0033",
     error500: "#9b095c",
-    success500: "#4BB543",
+    success500: "#4bb543",
     gray500: "#39324a",
     gray700: "#221c30",
     background: {
       primary: "#121212",
-      secondary: "#1E1E1E",
+      secondary: "#1e1e1e",
       tertiary: "#dcdcdc",
     },
     accent: {
+      primary50: "#8cffb8", // Lightest
       primary100: "#38ff8b",
-      primary500: "#27AE60",
-      secondary: "#2980B9",
+      primary200: "#2df578",
+      primary300: "#23ea66",
+      primary400: "#1ad953",
+      primary500: "#27AE60", // Base
+      primary600: "#20964d",
+      primary700: "#197e3a",
+      primary800: "#126527",
+      primary900: "#0b4c14", // Darkest
+
+      // Secondary shades
+      secondary50: "#92b2d1", // Lightest
+      secondary100: "#7b9cbc",
+      secondary200: "#6487a6",
+      secondary300: "#4e7291",
+      secondary400: "#39607b",
+      secondary500: "#2980B9", // Base
+      secondary600: "#1f6aa1",
+      secondary700: "#165488",
+      secondary800: "#0e3e6f",
+      secondary900: "#072855",
     },
     text: {
-      primary: "#EDEDED",
-      secondary: "#b9b9b9",
+      primary: "#ededed",
+      secondary: "#bdc3c7",
     },
   },
 };
