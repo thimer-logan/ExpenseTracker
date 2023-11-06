f78953d767d69fd5c53111929b7288561c80a3b9
Logan Thimer <thimerlogan@gmail.com>
2023-11-05 20:12:49 -0700

    Created icon map for the categories

---
+++ constants/icons.js
@@ -0,0 +1,27 @@
+export const categoryIconMap = {
+  Housing: { name: "house", family: "MaterialIcons" },
+  "Auto & Transportation": { name: "car-outline", family: "Ionicons" },
+  "Food & Grocery": { name: "food-outline", family: "MaterialCommunityIcons" },
+  "Health & Wellness": { name: "health-and-safety", family: "MaterialIcons" },
+  Entertainment: { name: "party-popper", family: "MaterialCommunityIcons" },
+  "Shopping & Personal Care": {
+    name: "shopping-outline",
+    family: "MaterialCommunityIcons",
+  },
+  Education: {
+    name: "book-education-outline",
+    family: "MaterialCommunityIcons",
+  },
+  "Travel & Vacations": { name: "travel-explore", family: "MaterialIcons" },
+  "Investment & Savings": { name: "savings", family: "MaterialIcons" },
+  "Debt & Loans": { name: "bank", family: "FontAwesome" },
+  "Family & Personal": { name: "person", family: "Octicons" },
+  "Bills & Utilities": {
+    name: "tools",
+    family: "MaterialCommunityIcons",
+  },
+  "Miscellaneous & Others": {
+    name: "miscellaneous-services",
+    family: "MaterialIcons",
+  },
+};
