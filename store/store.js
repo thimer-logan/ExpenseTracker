a26648d1b0a15ac15e1a23b99f18c10b0d037cda
Logan Thimer <thimerlogan@gmail.com>
2023-11-12 08:00:25 -0700

    Added budget to redux store

--- store/store.js
+++ store/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
 import expensesReducer from "./expenses";
 import authReducer from "./auth";
+import budgetReducer from "./budgets";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 export const store = configureStore(
@@ -9,6 +10,7 @@ export const store = configureStore(
     reducer: {
       expenses: expensesReducer,
       auth: authReducer,
+      budgets: budgetReducer,
     },
   },
   composeWithDevTools(applyMiddleware(thunkMiddleware))
